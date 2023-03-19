5825436808d2af25d7755820f46d5796824df240
mknyambura <kariuki.mercie@gmail.com>
2023-03-19 23:16:02 +0300

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+
+    def show
+        users = User.find_by(id: params[:id])
+        render json: users, include: :items
+    end
 end
